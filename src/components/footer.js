52625a62bd7eee8dd78a60fa00688d069b972aa1
philani-msibi-module-2 <philanims@tfg.co.za>
2022-06-19 23:40:55 +0200

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+
+        <footer>
+            <div>
+                <div className="social">
+                    <i class ="fab fa-facebook"></i>
+                    <i class ="fab fa-twitter"></i>
+                    <i class ="fab fa-linkedin"></i>
+                    <i class ="fab fa-instagram"></i>
+
+                </div>
+                <div className="copyright">
+               
+                    copyright &copy; 2022. SovTech
+
+                </div>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
---
+++ src/components/main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Main =() =>{
+    return(
+
+        <div className="Site-Info">
+            <div className="Title">
+                SovTech | Custom Software Development
+            </div>
+            <div className="Sub-Title">
+                Philani Msibi | SovTech Software developer Graduate 2022
+            </div>
+            <div className="message">
+    As a Graduate, I need a platform to start my career I have gone through SovTech profile this company encourage fresher talent to provide a chance to build an experience and skill which is helpful in different field area to judge and solve a difficulty so I want to join SovTech.
+      I am looking for a company, where I can apply the knowledge, my skills and insights that I have gained during my University days.
+      
+      A company that promotes and allows fresh minds to grow with a supportive back and encouraging push.SovTech is well known in this category.
+      Furthermore, it has different working positions which match my abilities and interests, so, I see a good career in working with Sovtech.
+
+      Everyone wants to work with the best organization to grow themselves as well as can explore their skills and make it more strong.
+      
+      I have searched that SovTech is a most ever fast-growing sector and a stable sector.
+      
+      It promises career growth and opportunity and It explores globally.
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Main;
